ef0bee2bf79580b623fd26a70ad4c5aec3acd030
ferluchin <lgrandab@gmail.com>
2023-01-08 20:15:16 -0500

    data.js
    changes to display images

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 const characterData = {
     hero: {
         name: "Wizard",
+        // avatar: "./images/wizard.png",  // local path
         avatar: "https://raw.githubusercontent.com/ferluchin/javascript-rpg/ce694b63f0a83ec42ea3e831d8bd0c27d00c604f/images/wizard.png",
         health: 60,
         diceCount: 3,
@@ -8,21 +9,24 @@ const characterData = {
     },
     orc: {
         name: "Orc",
-        avatar: "public/orc.png",
+        // avatar: "public/orc.png",
+        avatar: "https://github.com/ferluchin/javascript-rpg/blob/ce694b63f0a83ec42ea3e831d8bd0c27d00c604f/images/orc.png",
         health: 30,
         diceCount: 1,
         currentDiceScore: []
     },
     demon: {
         name: "Demon",
-        avatar: "./images/demon.png",
+        // avatar: "./images/demon.png",
+        avatar: "https://github.com/ferluchin/javascript-rpg/blob/ce694b63f0a83ec42ea3e831d8bd0c27d00c604f/images/demon.png",
         health: 25,
         diceCount: 2,
         currentDiceScore: []
     },
     goblin: {
         name: "Goblin",
-        avatar: "./images/goblin.png",
+        // avatar: "./images/goblin.png",
+        avatar: "https://github.com/ferluchin/javascript-rpg/blob/ce694b63f0a83ec42ea3e831d8bd0c27d00c604f/images/goblin.png",
         health: 20,
         diceCount: 3,
         currentDiceScore: []

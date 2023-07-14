7985698711f9f4c1d537891b0e867486ce5a2291
ferluchin <lgrandab@gmail.com>
2023-07-14 13:59:32 -0500

    fixed images !!!

--- data.js
+++ data.js
@@ -8,21 +8,21 @@ const characterData = {
     },
     orc: {
         name: "Orc",
-        avatar: "public/images/orc.png",
+        avatar: "images/orc.png",
         health: 30,
         diceCount: 1,
         currentDiceScore: []
     },
     demon: {
         name: "Demon",
-        avatar: "public/images/demon.png",
+        avatar: "images/demon.png",
         health: 25,
         diceCount: 2,
         currentDiceScore: []
     },
     goblin: {
         name: "Goblin",
-        avatar: "public/images/goblin.png",
+        avatar: "images/goblin.png",
         health: 20,
         diceCount: 3,
         currentDiceScore: []

8fd618ed6fd2cb788fdea0a8c3935587a9169952
ferluchin <lgrandab@gmail.com>
2023-07-14 13:56:53 -0500

    public images

--- data.js
+++ data.js
@@ -1,28 +1,28 @@
 const characterData = {
     hero: {
         name: "Wizard",
-        avatar: "images/wizard.png",
+        avatar: "public/images/wizard.png",
         health: 60,
         diceCount: 3,
         currentDiceScore: []
     },
     orc: {
         name: "Orc",
-        avatar: "images/orc.png",
+        avatar: "public/images/orc.png",
         health: 30,
         diceCount: 1,
         currentDiceScore: []
     },
     demon: {
         name: "Demon",
-        avatar: "images/demon.png",
+        avatar: "public/images/demon.png",
         health: 25,
         diceCount: 2,
         currentDiceScore: []
     },
     goblin: {
         name: "Goblin",
-        avatar: "images/goblin.png",
+        avatar: "public/images/goblin.png",
         health: 20,
         diceCount: 3,
         currentDiceScore: []
